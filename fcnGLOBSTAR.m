ee168f4c41896beeb3381bfa49d0e25a2b14bf47
Travis <Travis@10.16.112.248>
2016-09-27 22:07:58 -0400

    created fcnSTARGLOB and fcnGLOBSTAR for transformations between global and local reference frames

---
+++ fcnGLOBSTAR.m
@@ -0,0 +1,41 @@
+function [xsi] = fcnGLOBSTAR(points, roll, pitch, yaw)
+% Transforms a point from global to local reference frame
+% Input in RADIANS
+
+% INPUT:
+%   points - n x 3 matrix of (x,y,z) points in global coordinates
+%   roll - n x 1 vector of roll (nu) angles in radians
+%   pitch - n x 1 vector of pitch (eps) angles in radians
+%   yaw - n x 1 vector of yaw (psi) angles in radians
+% OUTPUT:
+%   xsi - n x 3 matrix of "points" in local reference frame
+
+x = reshape(points,3,1,[]);
+
+[A,B,~] = size(x);
+
+cnu = cos(roll);
+snu = sin(roll);
+ceps = cos(pitch);
+seps = sin(pitch);
+cpsi = cos(yaw);
+spsi = sin(yaw);
+
+rot(1,1,:) = cpsi.*ceps;
+rot(1,2,:) = cpsi.*seps.*snu+spsi.*cnu;
+rot(1,3,:) = -cpsi.*seps.*cnu+spsi.*snu;
+rot(2,1,:) = -spsi.*ceps;
+rot(2,2,:) = -spsi.*seps.*snu+cpsi.*cnu;
+rot(2,3,:) = spsi.*seps.*cnu+cpsi.*snu;
+rot(3,1,:) = seps;
+rot(3,2,:) = -ceps.*snu;
+rot(3,3,:) = ceps.*cnu;
+
+xsi(:,:,1) = x(:,:,1).*reshape(rot(1,1,:),A,B) + x(:,:,2).*reshape(rot(1,2,:),A,B) + x(:,:,3).*reshape(rot(1,3,:),A,B);
+xsi(:,:,2) = x(:,:,1).*reshape(rot(2,1,:),A,B) + x(:,:,2).*reshape(rot(2,2,:),A,B) + x(:,:,3).*reshape(rot(2,3,:),A,B);
+xsi(:,:,3) = x(:,:,1).*reshape(rot(3,1,:),A,B) + x(:,:,2).*reshape(rot(3,2,:),A,B) + x(:,:,3).*reshape(rot(3,3,:),A,B);
+
+xsi = reshape(xsi,[],3,1);
+
+end
+
