39cd8df0d199804f4660359c471b2835f366cb1f
travis-k <tkrebs@ryerson.ca>
2016-09-30 01:48:07 -0400

    housekeeping

--- VAP_MAIN.m
+++ VAP_MAIN.m
@@ -72,7 +72,7 @@ for ai = 1:length(seqALPHA)
         
         % Determining freestream vector
         vecUINF = fcnUINFWING(valALPHA, valBETA);
- 
+        
         % Initializing wake parameters
         matWAKEGEOM = [];
         matNPWAKEGEOM = [];
@@ -102,8 +102,8 @@ for ai = 1:length(seqALPHA)
         
         % Building wing resultant
         [vecR] = fcnRWING(valNELE, 0, matCENTER, matDVENORM, vecUINF, valWNELE, matWDVE, ...
-                    matWVLST, matWCOEFF, vecWK, vecWDVEHVSPN, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
-                    vecWDVETESWP, vecSYM, valWSIZE);
+            matWVLST, matWCOEFF, vecWK, vecWDVEHVSPN, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
+            vecWDVETESWP, vecSYM, valWSIZE);
         
         % Solving for wing coefficients
         [matCOEFF] = fcnSOLVED(matD, vecR, valNELE);
@@ -121,11 +121,11 @@ for ai = 1:length(seqALPHA)
             %   Calculate induced drag
             %   Calculate cn, cl, cy, cdi
             %   Calculate viscous effects
-            
+            tic
             %% Moving the wing
             [matVLST, matCENTER, matNEWWAKE, matNPNEWWAKE] = fcnMOVEWING(valALPHA, valBETA, valDELTIME, matVLST, matCENTER, matDVE, vecDVETE, matNPVLST);
             
-            % tic
+            
             
             %% Generating new wake elements
             [matWAKEGEOM, matNPWAKEGEOM, vecWDVEHVSPN, vecWDVEHVCRD, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
@@ -136,48 +136,54 @@ for ai = 1:length(seqALPHA)
             
             %% Creating and solving WD-Matrix for latest row of wake elements
             % We need to grab from matWADJE only the values we need for this latest row of wake DVEs
-            idx = sparse(sum(ismember(matWADJE,[((valWNELE - valWSIZE) + 1):valWNELE]'),2)>0 & (matWADJE(:,2) == 4 | matWADJE(:,2) == 2));            
+            idx = sparse(sum(ismember(matWADJE,[((valWNELE - valWSIZE) + 1):valWNELE]'),2)>0 & (matWADJE(:,2) == 4 | matWADJE(:,2) == 2));
             temp_WADJE = [matWADJE(idx,1) - (valTIMESTEP-1)*valWSIZE matWADJE(idx,2) matWADJE(idx,3) - (valTIMESTEP-1)*valWSIZE];
-          
+            
             [matWD, vecWR] = fcnWDWAKE([1:valWSIZE]', temp_WADJE, vecWDVEHVSPN(end-valWSIZE+1:end), vecWDVESYM(end-valWSIZE+1:end), vecWDVETIP(end-valWSIZE+1:end), vecWKGAM(end-valWSIZE+1:end));
             [matWCOEFF(end-valWSIZE+1:end,:)] = fcnSOLVEWD(matWD, vecWR, valWSIZE, vecWKGAM(end-valWSIZE+1:end), vecWDVEHVSPN(end-valWSIZE+1:end));
             
             %% Rebuilding and solving wing resultant
             [vecR] = fcnRWING(valNELE, valTIMESTEP, matCENTER, matDVENORM, vecUINF, valWNELE, matWDVE, ...
-                        matWVLST, matWCOEFF, vecWK, vecWDVEHVSPN, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
-                        vecWDVETESWP, vecSYM, valWSIZE);
+                matWVLST, matWCOEFF, vecWK, vecWDVEHVSPN, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
+                vecWDVETESWP, vecSYM, valWSIZE);
             [matCOEFF] = fcnSOLVED(matD, vecR, valNELE);
             
             %% Creating and solving WD-Matrix
             [matWD, vecWR] = fcnWDWAKE([1:valWNELE]', matWADJE, vecWDVEHVSPN, vecWDVESYM, vecWDVETIP, vecWKGAM);
             [matWCOEFF] = fcnSOLVEWD(matWD, vecWR, valWNELE, vecWKGAM, vecWDVEHVSPN);
             
-            % eltime(valTIMESTEP) = toc;
+            eltime(valTIMESTEP) = toc;
+            ttime(valTIMESTEP) = sum(eltime);
             
         end
     end
 end
 
-%% Plotting Wing
+%% Plotting
 
 if flagPLOT == 1
     [hFig2] = fcnPLOTBODY(1, valNELE, matDVE, matVLST, matCENTER);
     [hFig2] = fcnPLOTWAKE(1, hFig2, valWNELE, matWDVE, matWVLST, matWCENTER);
     [hLogo] = fcnPLOTLOGO(0.97,0.03,14,'k','none');
+    
+%     figure(1);
+%     plot(1:valTIMESTEP, eltime)
+%     xlabel('Timestep','FontSize',15)
+%     ylabel('Time per timestep (s)', 'FontSize',15)
+%     box on
+%     grid on
+%     axis tight
+%     
+%     figure(3);
+%     plot(1:valTIMESTEP, ttime)
+%     xlabel('Timestep','FontSize',15)
+%     ylabel('Total time (s)', 'FontSize',15)
+%     box on
+%     grid on
+%     axis tight
+    
 end
 
 %% Viscous wrapper
-
-toc
-%
-% figure(1);
-% plot(1:valTIMESTEP, eltime)
-% xlabel('Timestep','FontSize',15)
-% ylabel('Time per timestep (s)', 'FontSize',15)
-% box on
-% grid on
-% axis tight
-
-
 
 % whos
