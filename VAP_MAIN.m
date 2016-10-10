8441802e93c93395e2ca23f1fd482320c14894f6
travis-k <tkrebs@ryerson.ca>
2016-10-10 18:43:21 -0400

    corrections to circulation in the wake

--- VAP_MAIN.m
+++ VAP_MAIN.m
@@ -34,6 +34,7 @@ strFILE = 'inputs/VAP christmas.txt';
 
 vecM = [3 3 3 3 3 3 2]';
 seqALPHA = [5 7];
+
 % strFILE = 'inputs/input.txt';
 % strFILE = 'inputs/Config 1.txt';
 % strFILE = 'inputs/Config 2.txt';
@@ -46,7 +47,7 @@ seqALPHA = [5 7];
 
 
 valMAXTIME = 10;
-flagRELAX = 0;
+flagRELAX = 1;
 
 flagPLOT = 1;
 flagVERBOSE = 1;
@@ -149,7 +150,7 @@ for ai = 1:length(seqALPHA);
                 vecWDVEMCSWP, vecWDVETESWP, vecWDVEAREA, matWDVENORM, matWVLST, matWDVE, valWNELE, matWCENTER, matWCOEFF, vecWK, matWADJE, matNPVLST, vecWDVEPANEL, valLENWADJE, vecWDVESYM, vecWDVETIP, vecWKGAM, vecWDVEWING] ...
                 = fcnCREATEWAKEROW(matNEWWAKE, matNPNEWWAKE, matWAKEGEOM, matNPWAKEGEOM, vecWDVEHVSPN, vecWDVEHVCRD, vecWDVEROLL, vecWDVEPITCH, vecWDVEYAW, vecWDVELESWP, ...
                 vecWDVEMCSWP, vecWDVETESWP, vecWDVEAREA, matWDVENORM, matWVLST, matWDVE, valWNELE, matWCENTER, matWCOEFF, vecWK, matCOEFF, vecDVETE, matWADJE, matNPVLST, vecDVEPANEL, ...
-                vecWDVEPANEL, vecSYM, valLENWADJE, vecWKGAM, vecWDVESYM, vecWDVETIP, vecK, vecDVEWING, vecWDVEWING);
+                vecWDVEPANEL, vecSYM, valLENWADJE, vecWKGAM, vecWDVESYM, vecWDVETIP, vecK, vecDVEWING, vecWDVEWING, flagSTEADY, valWSIZE);
             
             %% Creating and solving WD-Matrix for latest row of wake elements
             % We need to grab from matWADJE only the values we need for this latest row of wake DVEs
@@ -203,10 +204,12 @@ for ai = 1:length(seqALPHA);
         tic
         %% Viscous wrapper
         valCL = vecCL(end,ai);
+        valCDI = vecCDI(end,ai);
         
         q_inf = valWEIGHT/(valCL*valAREA);
         valVINF = sqrt(2*q_inf/valDENSITY);
-                
+        di = valCDI*valAREA*q_inf;
+        
         % Summing freestream and induced forces of each DVE
         % This will NOT work with rotors, it does not take into
         % account freestream! UINF^2*AREA should be the denominator
@@ -222,6 +225,8 @@ for ai = 1:length(seqALPHA);
         vecCYDIST = [];
         matXYZDIST = [];
         matLEDVEDIST = [];
+        vecREDIST = [];
+        vecAREADIST = [];
         
         for i = 1:max(vecDVEWING)
             
@@ -236,6 +241,7 @@ for ai = 1:length(seqALPHA);
             end
             
             m = m(1);
+            len = length(vecCLDIST); % start point for this panel in the vectors
             
             % Matrix of how much we need to add to an index to get the next chordwise element
             % It is done this way because n can be different for each panel. Unlike in the wake,
@@ -254,10 +260,11 @@ for ai = 1:length(seqALPHA);
             
             % The leading edge DVE for the distribution
             matLEDVEDIST = [matLEDVEDIST; idxdve];
-        
-            %% Viscous effects
             
-            re = valVINF.*2.*mean(vecDVEHVCRD(rows),2)./valKINV;
+            %% Wing/horizontal stabilizer lift and drag
+            
+            vecREDIST = [vecREDIST; valVINF.*2.*sum(vecDVEHVCRD(rows),2)./valKINV];
+            vecAREADIST = [vecAREADIST; sum(vecDVEAREA(rows),2)];
             
             for j = 1:length(idxpanel)
                 pan = idxpanel(j);
@@ -266,11 +273,113 @@ for ai = 1:length(seqALPHA);
                 HiRe = airfoil(end,4);
                 LoRe = airfoil(1,4);
                 
-%                 cl = vecDVECN
+                cl = vecCLDIST(len + j);
+                
+                if vecREDIST(len + j) > HiRe
+                    if flagVERBOSE == 1
+                        disp('Re higher than airfoil Re data')
+                    end
+                    cl_max = airfoil(end,4);
+                elseif vecREDIST(len + j) < LoRe
+                    if flagVERBOSE == 1
+                        disp('Re lower than airfoil Re data');
+                    end
+                    Re2 = airfoil(1,4);
+                    temp_var = airfoil(airfoil(:,4) == Re2, 2);
+                    cl_max = temp_var(end);
+                else
+                    re1 = airfoil(airfoil(:,4) < vecREDIST(len + j), 4);
+                    re1 = re1(end);
+                    cl_max1 = airfoil(airfoil(:,4) < vecREDIST(len + j), 2);
+                    cl_max1 = cl_max1(end);
+                    
+                    temp_var = airfoil(airfoil(:,4) > vecREDIST(len + j),4);
+                    re2 = temp_var(1);
+                    temp_var = airfoil(airfoil(:,4) == (temp_var(1)), 2);
+                    cl_max2 = temp_var(end);
+                    
+                    cl_max = interp1([re1 re2],[cl_max1 cl_max2], vecREDIST(len + j));
+                end
+                
+                % correcting the section cl if we are above cl_max
+                if cl > cl_max
+                    if flagVERBOSE == 1
+                        fprintf('\nStall of Wing %d Section %d, cl = %f Re = %0.0f', i, j, cl, vecREDIST(len + j))
+                    end
+                    vecCLDIST(len + j) = 0.825*cl_max; % setting the stalled 2d cl
+                end
+                
+                F = scatteredInterpolant(airfoil(:,4), airfoil(:,2), airfoil(:,3),'nearest');
+                vecCDPDIST(len + j, 1) = F(vecREDIST(len + j), cl);
+                
+                % Octave:
+                % vecCDPDIST(len + j, 1) = griddata(airfoil(:,4), airfoil(:,2), airfoil(:,3), vecREDIST(len + j), cl, 'nearest');
                 
             end
+        end
+        
+        dprof = sum(vecCDPDIST.*q_inf.*vecAREADIST);
+        
+        % This function does not account for symmetry well, it is all or nothing with symmetry,
+        % but it really should be wing-by-wing
+        if any(vecSYM) == 1
+            dprof = 2.*dprof;
+        end
+        
+        %% Vertical tail drag
+        
+        dvt = 0;
+        for ii = 1:valVSPANELS
+            Re = valVINF*matVSGEOM(ii,2)/valKINV;
             
-        end 
+            % Load airfoil data
+            airfoil = dlmread(strcat('airfoils/airfoil',num2str(matVSGEOM(ii,4)),'.dat'),'\t', 1, 0);
+            
+            % determining the drag coefficient corresponding to lift
+            % coefficient of 0
+            
+            % MATLAB:
+            F = scatteredInterpolant(airfoil(:,4), airfoil(:,2), airfoil(:,3),'nearest');
+            cdvt = F(Re, 0);
+            % Octave:
+            % cdvt = griddata(Temp.Airfoil(:,4), Temp.Airfoil(:,2), Temp.Airfoil(:,3), Re, 0, 'nearest');
+            
+            dvt = dvt + cdvt*matVSGEOM(ii,3);
+        end
+        
+        dvt = dvt*q_inf;
+        
+        %% Fuselage drag
+        
+        dfuselage = 0;
+        
+        tempSS = valVINF*valFPWIDTH/valKINV;
+        
+        for ii = 1:valFPANELS
+            Re_fus = (ii-0.5)*tempSS;
+            if ii < valFTURB
+                cdf = 0.664/sqrt(Re_fus); % Laminar
+            else
+                cdf = 0.0576/(Re_fus^0.2); % Turbulent
+            end
+            
+            dfuselage = dfuselage + cdf*matFGEOM(ii,2)*pi*valFPWIDTH;
+        end
+        
+        dfuselage = dfuselage*q_inf;
+        
+        %% Total Drag
+        
+        valD = di + dprof + dvt + dfuselage;
+        
+        dint = valD*(valINTERF/100);
+        
+        valD = valD + dint;
+        
+        
+        
+        
+        
         toc
     end
 end
