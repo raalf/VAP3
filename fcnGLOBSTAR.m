7df198c97462aa22cdb6437d120098e9ee5edd98
Alton-Y <alton.yeung@ryerson.ca>
2016-10-12 14:11:42 -0400

    "speed boost STARGLOB and GLOBSTAR"

--- fcnGLOBSTAR.m
+++ fcnGLOBSTAR.m
@@ -12,13 +12,6 @@ function [xsi] = fcnGLOBSTAR(points, roll, pitch, yaw)
 
 len = length(points(:,1));
 
-rot = zeros(3,3,len);
-xsi = zeros(len,1,3);
-
-x = permute(points,[1 3 2]);
-
-[A,B,~] = size(x);
-
 cnu = cos(roll);
 snu = sin(roll);
 ceps = cos(pitch);
@@ -26,25 +19,10 @@ seps = sin(pitch);
 cpsi = cos(yaw);
 spsi = sin(yaw);
 
-% rot(1,1,:) = cpsi.*ceps;
-% rot(1,2,:) = cpsi.*seps.*snu+spsi.*cnu;
-% rot(1,3,:) = -cpsi.*seps.*cnu+spsi.*snu;
-% rot(2,1,:) = -spsi.*ceps;
-% rot(2,2,:) = -spsi.*seps.*snu+cpsi.*cnu;
-% rot(2,3,:) = spsi.*seps.*cnu+cpsi.*snu;
-% rot(3,1,:) = seps;
-% rot(3,2,:) = -ceps.*snu;
-% rot(3,3,:) = ceps.*cnu;
-% 
-% xsi(:,:,1) = x(:,:,1).*reshape(rot(1,1,:),A,B) + x(:,:,2).*reshape(rot(1,2,:),A,B) + x(:,:,3).*reshape(rot(1,3,:),A,B);
-% xsi(:,:,2) = x(:,:,1).*reshape(rot(2,1,:),A,B) + x(:,:,2).*reshape(rot(2,2,:),A,B) + x(:,:,3).*reshape(rot(2,3,:),A,B);
-% xsi(:,:,3) = x(:,:,1).*reshape(rot(3,1,:),A,B) + x(:,:,2).*reshape(rot(3,2,:),A,B) + x(:,:,3).*reshape(rot(3,3,:),A,B);
-
-xsi(:,:,1) = x(:,:,1).*reshape(cpsi.*ceps,A,B) + x(:,:,2).*reshape(cpsi.*seps.*snu+spsi.*cnu,A,B) + x(:,:,3).*reshape(-cpsi.*seps.*cnu+spsi.*snu,A,B);
-xsi(:,:,2) = x(:,:,1).*reshape(-spsi.*ceps,A,B) + x(:,:,2).*reshape(-spsi.*seps.*snu+cpsi.*cnu,A,B) + x(:,:,3).*reshape(spsi.*seps.*cnu+cpsi.*snu,A,B);
-xsi(:,:,3) = x(:,:,1).*reshape(seps,A,B) + x(:,:,2).*reshape(-ceps.*snu,A,B) + x(:,:,3).*reshape(ceps.*cnu,A,B);
-
-xsi = reshape(xsi,[],3,1);
+xsi = zeros(len,3);
+xsi(:,1) = points(:,1).*(cpsi.*ceps) + points(:,2).*(cpsi.*seps.*snu+spsi.*cnu) + points(:,3).*(-cpsi.*seps.*cnu+spsi.*snu);
+xsi(:,2) = points(:,1).*(-spsi.*ceps) + points(:,2).*(-spsi.*seps.*snu+cpsi.*cnu) + points(:,3).*(spsi.*seps.*cnu+cpsi.*snu);
+xsi(:,3) = points(:,1).*(seps) + points(:,2).*(-ceps.*snu) + points(:,3).*(ceps.*cnu);
 
 end
 
--- fcnSTARGLOB.m
+++ fcnSTARGLOB.m
@@ -11,13 +11,6 @@ function [xsi] = fcnSTARGLOB(points, roll, pitch, yaw)
 %   xsi - n x 3 matrix of "points" in global reference frame
 
 len = length(points(:,1));
-rot = zeros(3,3,len);
-
-xsi = zeros(len,1,3);
-
-x = permute(points,[1 3 2]);
-
-[A,B,~] = size(x);
 
 cnu = cos(roll);
 snu = sin(roll);
@@ -26,25 +19,10 @@ seps = sin(pitch);
 cpsi = cos(yaw);
 spsi = sin(yaw);
 
-% rot(1,1,:) = cpsi.*ceps;
-% rot(1,2,:) = cpsi.*seps.*snu+spsi.*cnu;
-% rot(1,3,:) = -cpsi.*seps.*cnu+spsi.*snu;
-% rot(2,1,:) = -spsi.*ceps;
-% rot(2,2,:) = -spsi.*seps.*snu+cpsi.*cnu;
-% rot(2,3,:) = spsi.*seps.*cnu+cpsi.*snu;
-% rot(3,1,:) = seps;
-% rot(3,2,:) = -ceps.*snu;
-% rot(3,3,:) = ceps.*cnu;
-
-% xsi(:,:,1) = x(:,:,1).*reshape(rot(1,1,:),A,B) + x(:,:,2).*reshape(rot(2,1,:),A,B) + x(:,:,3).*reshape(rot(3,1,:),A,B);
-% xsi(:,:,2) = x(:,:,1).*reshape(rot(1,2,:),A,B) + x(:,:,2).*reshape(rot(2,2,:),A,B) + x(:,:,3).*reshape(rot(3,2,:),A,B);
-% xsi(:,:,3) = x(:,:,1).*reshape(rot(1,3,:),A,B) + x(:,:,2).*reshape(rot(2,3,:),A,B) + x(:,:,3).*reshape(rot(3,3,:),A,B);
-
-xsi(:,:,1) = x(:,:,1).*reshape(cpsi.*ceps,A,B) + x(:,:,2).*reshape(-spsi.*ceps,A,B) + x(:,:,3).*reshape(seps,A,B);
-xsi(:,:,2) = x(:,:,1).*reshape(cpsi.*seps.*snu+spsi.*cnu,A,B) + x(:,:,2).*reshape(-spsi.*seps.*snu+cpsi.*cnu,A,B) + x(:,:,3).*reshape(-ceps.*snu,A,B);
-xsi(:,:,3) = x(:,:,1).*reshape(-cpsi.*seps.*cnu+spsi.*snu,A,B) + x(:,:,2).*reshape(spsi.*seps.*cnu+cpsi.*snu,A,B) + x(:,:,3).*reshape(ceps.*cnu,A,B);
-
-xsi = reshape(xsi,[],3,1);
+xsi = zeros(len,3);
+xsi(:,1) = points(:,1).*(cpsi.*ceps) + points(:,2).*(-spsi.*ceps) + points(:,3).*(seps);
+xsi(:,2) = points(:,1).*(cpsi.*seps.*snu+spsi.*cnu) + points(:,2).*(-spsi.*seps.*snu+cpsi.*cnu) + points(:,3).*(-ceps.*snu);
+xsi(:,3) = points(:,1).*(-cpsi.*seps.*cnu+spsi.*snu) + points(:,2).*(spsi.*seps.*cnu+cpsi.*snu) + points(:,3).*(ceps.*cnu);
 
 end
 
