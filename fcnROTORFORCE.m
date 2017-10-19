d1ebe2430b7711af5d90cda045707c2b81dfafd5
devinbarcelos <devinbarcelos@gmail.com>
2017-10-19 16:37:42 -0400

    Opps

---
+++ fcnROTORFORCE.m
@@ -0,0 +1,28 @@
+function [ et ] = fcnROTORFORCE( en, vecDVENFREE, vecDVENIND, inddrag, matUINF, vecDVEROTOR, matVEHROT, matROTORAXIS, vecROTORRPM, vecROTDIAM, matCENTER)
+
+% Thrust direction in global reference frame
+et = repmat(fcnSTARGLOB(matROTORAXIS, matVEHROT(:,1), matVEHROT(:,2), matVEHROT(:,3)),size(vecDVENFREE,1),1);
+
+% % Make induced drag same length as other distributions (for m>1)
+% tempDi = zeros(size(vecDVENFREE,1),1);
+% tempDi(vecDVETE==3) = inddrag;
+
+% Velocitiy direction
+matVELDIR = matUINF./(sqrt(matUINF(:,1).^2+matUINF(:,2).^2+matUINF(:,3).^2));
+
+% Force distributions
+vecTHRUSTDIST = dot(vecDVENFREE.*en,et,2) + dot(vecDVENIND.*en,et,2) + dot(inddrag.*matUINF,et,2);
+
+
+for i = 1:max(vecDVEROTOR)
+    idx = vecDVEROTOR == i;
+    thrust(i) = sum(vecTHRUSTDIST(idx));
+end
+
+vecCT = thrust./(((vecROTORRPM/60)^2).*((vecROTDIAM).^4));
+
+% hold on
+% quiver3(matCENTER(:,1),matCENTER(:,2),matCENTER(:,3),en(:,1),en(:,2),en(:,3))
+% quiver3(matCENTER(:,1),matCENTER(:,2),matCENTER(:,3),matVELDIR(:,1),matVELDIR(:,2),matVELDIR(:,3))
+% quiver3(matCENTER(:,1),matCENTER(:,2),matCENTER(:,3),et(:,1),et(:,2),et(:,3))
+end
