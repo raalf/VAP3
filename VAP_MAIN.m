b77dce72a7b6f13f977ed913a0339dcd5d812bdf
devinbarcelos <devinbarcelos@gmail.com>
2018-03-09 12:13:51 -0500

    Updated the J Cole prop geom to account for zero lift line

--- VAP_MAIN.m
+++ VAP_MAIN.m
@@ -6,9 +6,11 @@ warning off
 filename = 'inputs/J_COLE_X57_CRUISE_PROP.vap'
 
 % seqALPHA = [2:1:15];
-vecCOLLECTIVE = [-50:5:15];
+vecCOLLECTIVE = [-50:2:10];
 % vecCOLLECTIVE = 0;
 
 parfor i = 1:length(vecCOLLECTIVE)
     OUTP(i) = fcnVAP_MAIN(filename, vecCOLLECTIVE(i));
 end
+
+save('VAP315ZLL_STEADY_VISCOUS_FIXED_CRUISE_PROP')
