d7746a7f2ff65dd51b08f441d02dbb9d134aa509
devinbarcelos <devinbarcelos@gmail.com>
2018-03-11 13:28:16 -0400

    Increased number of radial locations for the X57 cruise prop

--- VAP_MAIN.m
+++ VAP_MAIN.m
@@ -3,7 +3,8 @@ clear
 warning off
 
 % filename = 'inputs/J_COLE_BASELINE_WING.vap';
-filename = 'inputs/J_COLE_X57_CRUISE_PROP.vap'
+% filename = 'inputs/J_COLE_X57_CRUISE_PROP.vap'
+filename = 'inputs/J_COLE_X57_CRUISE_PROP_20_SECTIONS.vap'
 
 % seqALPHA = [2:1:15];
 vecCOLLECTIVE = [-50:2:10];
@@ -13,4 +14,4 @@ parfor i = 1:length(vecCOLLECTIVE)
     OUTP(i) = fcnVAP_MAIN(filename, vecCOLLECTIVE(i));
 end
 
-save('VAP315ZLL_STEADY_VISCOUS_FIXED_CRUISE_PROP')
+save('VAP315_STEADY_VISCOUS_FIXED_CRUISE_PROP_20RadialSections')
