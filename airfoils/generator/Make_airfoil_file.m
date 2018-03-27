6b42a728d6d34ffe5c2776bee18326c6b0c22ab0
Alton-Y <alton.yeung@ryerson.ca>
2018-03-26 22:28:19 -0400

    modified airfoil file generation

--- airfoils/generator/Make_airfoil_file.m
+++ airfoils/generator/Make_airfoil_file.m
@@ -5,4 +5,4 @@ clear
 airfoil_name = 'MH 117';
 coord = dlmread([airfoil_name, '.dat'],'',1,0);
 
-VAP3_airfoil_gen(airfoil_name, coord, [100000:150000:5e6])
+VAP3_airfoil_gen(airfoil_name, coord, [150000:150000:5e6], [-10:0.25:20])
--- airfoils/generator/VAP3_airfoil_gen.m
+++ airfoils/generator/VAP3_airfoil_gen.m
@@ -1,11 +1,11 @@
-function [] = VAP3_airfoil_gen(airfoil_name, coord, Re_range)
+function [] = VAP3_airfoil_gen(airfoil_name, coord, Re_range, Alpha_Range)
 
 ab = dir;
 if ~any(cellfun( @(x) strcmp(x,'aux_files'), {ab.name} ))
     mkdir aux_files;
 end
     
-pol = xfoil(coord, -2:0.5:15, Re_range, 10, [1 1], false);
+pol = xfoil(coord, Alpha_Range, Re_range, 10, [1 1], false);
 
 save(['out_',airfoil_name], 'pol', 'coord', 'Re_range', 'airfoil_name');
 
