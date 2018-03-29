d3df3baba5cb3eac1fbe69025a62baa7114be315
travis-k <tkrebs@ryerson.ca>
2018-03-29 18:24:58 -0400

    I think I implemented the wake induced velocity shortcut

--- fcnRWING.m
+++ fcnRWING.m
@@ -9,14 +9,17 @@ len = length(SURF.matCENTER(:,1));
 
 if valTIMESTEP < 1;
     % Flow tangency at control points goes at the bottom of the resultant
-    vecR(end-(len-1):end) = (4*pi).*dot(SURF.matUINF, SURF.matDVENORM,2);    
+    vecR(end-(len-1):end) = (4*pi).*dot(SURF.matUINF, SURF.matDVENORM,2);
 else
-%     w_wake = fcnWDVEVEL(SURF.matCENTER, valTIMESTEP, WAKE, SURF, FLAG);
-    [w_wake, WAKE] = fcnWDVEVEL_SHORTCUT([1:SURF.valNELE]', valTIMESTEP, WAKE, SURF, FLAG);
-
+    if FLAG.RELAX == 0
+        [w_wake, WAKE] = fcnWDVEVEL_SHORTCUT([1:SURF.valNELE]', valTIMESTEP, WAKE, SURF, FLAG);  
+    else
+        [w_wake] = fcnWDVEVEL(SURF.matCENTER, valTIMESTEP, WAKE, SURF, FLAG);
+    end
+    
     % Including the wake-induced velocities,
-    vecR(end-(len-1):end) = (4*pi).*dot(SURF.matUINF+w_wake, SURF.matDVENORM,2);  
-
+    vecR(end-(len-1):end) = (4*pi).*dot(SURF.matUINF+w_wake, SURF.matDVENORM,2);
+    
 end
 
 end
--- fcnWDVEVEL_SHORTCUT.m
+++ fcnWDVEVEL_SHORTCUT.m
@@ -1,41 +1,44 @@
 function [w_wake, WAKE] = fcnWDVEVEL_SHORTCUT(influencee, valTIMESTEP, WAKE, SURF, FLAG)
 % INPUT:
 %   influencee - DVE number of the influencee (influence is on this DVE's control point
-
-if isempty(WAKE.matWINDUC_REF)
+if valTIMESTEP == 1
     WAKE.matWINDUC_REF = nan(WAKE.valWSIZE, SURF.valNELE, 1);
+elseif valTIMESTEP == 2
+    WAKE.matWINDUC_REF = nan(WAKE.valWSIZE.*2, SURF.valNELE, 1);
 else
     % Upper 2 rows (oldest wake elements) are nan, because these are semi-infinite sheets and have to be recalculated
-    WAKE.matWINDUC_REF = [nan(WAKE.valWSIZE.*2, SURF.valNELE, 1); WAKE.matWINDUC_REF(2:end, :)];
+    WAKE.matWINDUC_REF = [nan(WAKE.valWSIZE.*2, SURF.valNELE, 1); WAKE.matWINDUC_REF(WAKE.valWSIZE+1:end, :)];
 end
 
-fpg = SURF.matCENTER;
 % List of wDVEs we are influencing from (each one for each of the fieldpoints)
-len = length(fpg(:,1));
+len_original = length(influencee);
+len = length(influencee);
 dvenum = reshape(repmat(1:WAKE.valWNELE,len,1),[],1);
+influencee = repmat(influencee, WAKE.valWNELE, 1);
 
+fpg = SURF.matCENTER;
 fpg = repmat(fpg,WAKE.valWNELE,1);
 
 if FLAG.STEADY == 1
     % DVE type 1 is a regular wake DVE
     dvetype = ones(length(dvenum),1);
-
+    
     % Newest row of wake DVEs have a filament at the leading edge
     if FLAG.TRI == 1
         newest_row = sort([WAKE.valWNELE:-2:WAKE.valWNELE-WAKE.valWSIZE*2+1]-1)';
     else
         newest_row = [((WAKE.valWNELE-WAKE.valWSIZE)+1):1:WAKE.valWNELE]';
     end
-
+    
     dvetype(ismember(dvenum, newest_row)) = 2;
-
+    
     % Oldest row of wake DVEs are semi-infinite
     if FLAG.TRI == 1
         oldest_row = [2:2:WAKE.valWSIZE*2];
     else
-    oldest_row = [1:WAKE.valWSIZE]';
+        oldest_row = [1:WAKE.valWSIZE]';
     end
-
+    
     if valTIMESTEP == 1 && FLAG.TRI == 0
         dvetype(ismember(dvenum, oldest_row)) = -3;
     elseif valTIMESTEP > 0 && FLAG.TRI == 0
@@ -43,11 +46,11 @@ if FLAG.STEADY == 1
     elseif FLAG.TRI == 1 % last row of wake elements in a tri-wake never has a bound vortex
         dvetype(ismember(dvenum, oldest_row)) = 3;
     end
-
+    
 elseif FLAG.STEADY == 0 || 2
     % DVE type 1 is a regular wake DVE
     dvetype = zeros(length(dvenum),1);
-
+    
     % Oldest row of wake DVEs are semi-infinite
     if FLAG.TRI == 1
         oldest_row = [2:2:WAKE.valWSIZE*2];
@@ -59,8 +62,45 @@ elseif FLAG.STEADY == 0 || 2
     
 end
 
-[w_ind] = fcnDVEVEL(dvenum, fpg, dvetype, WAKE.matWDVE, WAKE.matWVLST, WAKE.matWCOEFF, WAKE.vecWK, WAKE.vecWDVEHVSPN, WAKE.vecWDVEHVCRD, WAKE.vecWDVEROLL, WAKE.vecWDVEPITCH, WAKE.vecWDVEYAW, WAKE.vecWDVELESWP, WAKE.vecWDVETESWP, SURF.vecDVESYM, FLAG.GPU);
+%% Finding stored calculations
+len = length(dvenum);
+linearInd = sub2ind(size(WAKE.matWINDUC_REF), dvenum, influencee);
+stored = ~isnan(WAKE.matWINDUC_REF(linearInd));
+a = nan(len,3);
+b = nan(len,3);
+c = nan(len,3);
 
-w_wake = reshape(sum(reshape(w_ind', len*3, [])',1),3,[])';
+if valTIMESTEP > 1 && ~isempty(WAKE.matWINDUC)
+    a(stored,:) =  WAKE.matWINDUC(WAKE.matWINDUC_REF(linearInd(stored)),:,1);
+    b(stored,:) =  WAKE.matWINDUC(WAKE.matWINDUC_REF(linearInd(stored)),:,2);
+    c(stored,:) =  WAKE.matWINDUC(WAKE.matWINDUC_REF(linearInd(stored)),:,3);
+end
+
+%% Finding unstored computations
+[a(~stored,:), b(~stored,:), c(~stored,:)] = fcnDVEINF(dvenum(~stored), dvetype(~stored), fpg(~stored,:), WAKE.vecWK, ...
+    WAKE.matWDVE, WAKE.matWVLST, WAKE.vecWDVEHVSPN, WAKE.vecWDVEHVCRD, WAKE.vecWDVEROLL, WAKE.vecWDVEPITCH, ...
+    WAKE.vecWDVEYAW, WAKE.vecWDVELESWP, WAKE.vecWDVETESWP, SURF.vecDVESYM, FLAG.GPU);
+
+% Saving the influence coefficients
+if valTIMESTEP > 1
+    if isempty(WAKE.matWINDUC)
+        idx = 1:len;
+    else
+        idx = size(WAKE.matWINDUC, 1) + 1:size(WAKE.matWINDUC,1) + sum(~stored);
+    end
+    WAKE.matWINDUC_REF(sub2ind(size(WAKE.matWINDUC_REF), dvenum(~stored), influencee(~stored))) = idx;
+    WAKE.matWINDUC(idx,:,1) = a(~stored,:);
+    WAKE.matWINDUC(idx,:,2) = b(~stored,:);
+    WAKE.matWINDUC(idx,:,3) = c(~stored,:);
+end
+
+%% Finding the induced velocities
+D = [a b c];
+D = reshape(reshape(D', 1, 9, []), 3, 3, len);
+
+w_ind = permute(sum(D.*repmat(reshape(WAKE.matWCOEFF(dvenum,:)',1,3,[]),3,1,1),2),[2 1 3]);
+w_ind = reshape(permute(w_ind,[3 1 2]),[],3,1)./(-4*pi);
+w_wake = reshape(sum(reshape(w_ind', len_original*3, [])',1),3,[])';
+
 end
 
