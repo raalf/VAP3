eea83eb0d098b9530f4508af54349cd98b9f7dce
unknown <alton.yeung@ryerson.ca>
2018-04-19 00:28:20 -0400

    update jcole trim iter4 run

--- Runs/J_COLE_BASELINE_SYM/J_COLE_TrimmingStep4.m
+++ Runs/J_COLE_BASELINE_SYM/J_COLE_TrimmingStep4.m
@@ -15,7 +15,8 @@ CD3temp(isnan([OUTP3.vecCL])) = nan;
 CD3 = nanmean(CD3temp,1);
 
 
-PLOTON = 1;
+PLOTON = 0;
+
 if PLOTON == 1
     figure(1)
     plot(KTAS, CL, '-o')
@@ -28,7 +29,7 @@ if PLOTON == 1
     plot(KTAS, CL3, '-s')
     hold off
     legend('Wing Only','Wing + Prop Iter.1','Wing + Prop Iter.2','Wing + Prop Iter.3')
-    saveFig2Latex('JCole_Trim_Rev1_Iter3_CL.pdf', [10, 8])
+    %     saveFig2Latex('JCole_Trim_Rev1_Iter3_CL.pdf', [10, 8])
     
     figure(2)
     plot(KTAS, CD, '-o')
@@ -41,7 +42,7 @@ if PLOTON == 1
     plot(KTAS, CD3, '-s')
     hold off
     legend('Wing Only','Wing + Prop Iter.1','Wing + Prop Iter.2','Wing + Prop Iter.3')
-    saveFig2Latex('JCole_Trim_Rev1_Iter3_CD.pdf', [10, 8])
+    %     saveFig2Latex('JCole_Trim_Rev1_Iter3_CD.pdf', [10, 8])
     
     figure(3)
     plot(KTAS, CT, '-o')
@@ -55,8 +56,75 @@ if PLOTON == 1
     hold off
     legend('Wing Only','Wing + Prop Iter.1','Wing + Prop Iter.2','Wing + Prop Iter.3',...
         'Location','SouthEast')
-    saveFig2Latex('JCole_Trim_Rev1_Iter3_CT.pdf', [10, 8])
+    %     saveFig2Latex('JCole_Trim_Rev1_Iter3_CT.pdf', [10, 8])
 end
+
+
+
+% interpolate alpha to maintain steady level flight at VINF
+% using wing+prop data from iter.1 and 2
+seqALPHA4 = seqALPHA3*nan;
+vecCOLLECTIVE4 = vecCOLLECTIVE3*nan;
+
+if PLOTON == 1
+    figure(4)
+    clf
+    figure(5)
+    clf
+end
+
+for nn = 1:length(seqALPHA4)
+    % New sets of AOA input for 3rd iteration in order to hit the targeted CL
+    seqALPHA4(nn) = interp1([CL2(nn) CL3(nn)],...
+        [seqALPHA2(nn) seqALPHA3(nn)],...
+        CL(nn),'linear','extrap');
+    
+    % New sets of collective pitch input for 3rd iteration in order to hit the targeted CT
+    vecCOLLECTIVE4(nn) = interp1([CT2(nn) CT3(nn)],...
+        [vecCOLLECTIVE2(nn) vecCOLLECTIVE3(nn)],...
+        CT(nn),'linear','extrap');
+    
+    if PLOTON == 1
+        figure(4)
+        hold on
+        plot([seqALPHA(nn) seqALPHA2(nn) seqALPHA3(nn)],...
+            [CL1(nn) CL2(nn) CL3(nn)],'-o');
+        scatter(seqALPHA4(nn), CL(nn),'filled');
+        grid minor
+        hold off
+        
+        figure(5)
+        hold on
+        plot([vecCOLLECTIVE(nn) vecCOLLECTIVE2(nn) vecCOLLECTIVE3(nn)],...
+            [CT1(nn) CT2(nn) CT3(nn)],'-o');
+        scatter(vecCOLLECTIVE4(nn), CT(nn),'filled');
+        grid minor
+        hold off
+    end
+end
+
+
+
+% Running
+warning off
+filename = 'inputs/J_COLE_BASELINE_SYM.vap';
+parfor i = 1:length(vecCOLLECTIVE4)
+    VAP_IN = [];
+    VAP_IN.vecVEHALPHA = seqALPHA4(i);
+    VAP_IN.vecCOLLECTIVE = vecCOLLECTIVE4(i);
+    VAP_IN.vecVEHVINF = vecVEHVINF(i);
+    VAP_IN.valSTARTFORCES = 138;
+    VAP_IN.valMAXTIME = 160;
+    
+    OUTP(i) = fcnVAP_MAIN(filename, VAP_IN);
+
+    fprintf('finished AOA=%.1f\n',seqALPHA(i));
+end
+
+save('VAP32_WING+PROP_forCDo_TS160_fixed_last22Timesteps_4thTrimIter.mat')
+
+
+
 
 
 
