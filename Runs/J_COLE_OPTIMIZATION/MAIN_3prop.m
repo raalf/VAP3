4379165807bee0087f39f9ec369aa1c67478762d
travi <tkrebs@ryerson.ca>
2018-10-10 16:35:59 -0400

    JCOLE: Updating 3_prop size and returning chunk size to 4 million

--- Runs/J_COLE_OPTIMIZATION/MAIN_3prop.m
+++ Runs/J_COLE_OPTIMIZATION/MAIN_3prop.m
@@ -19,7 +19,7 @@ constraints_3prop;
 nvars = length(lb);
 % IntCon = 1:nvars;
 IntCon = [];
-[seeds, max_tip_speed, min_tip_speed] = creation(nvars,N_prop,200,ub,lb,A,b,N_chord,N_dihe,Vars_prop);
+[seeds, max_tip_speed, min_tip_speed] = creation(nvars,N_prop,160,ub,lb,A,b,N_chord,N_dihe,Vars_prop);
 
-options = optimoptions('ga', 'Display', 'iter', 'InitialPopulation',seeds,'UseParallel', true, 'MaxGenerations', 1000, 'StallGenLimit', 50, 'MutationFcn','mutationadaptfeasible', 'CreationFcn', 'gacreationlinearfeasible');
+options = optimoptions('ga', 'Display', 'iter', 'InitialPopulation',seeds,'UseParallel', true, 'MaxGenerations', 1000, 'PopulationSize', 160, 'StallGenLimit', 50, 'MutationFcn','mutationadaptfeasible', 'CreationFcn', 'gacreationlinearfeasible');
 [x,fval,exitflag,output,population,scores] = ga({@fcnOBJECTIVE, N_chord, N_dihe, N_prop, Vars_prop, max_tip_speed, min_tip_speed},nvars,A,b,[],[],lb,ub,[],IntCon,options);
