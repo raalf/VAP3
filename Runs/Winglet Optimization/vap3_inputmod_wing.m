a06072c99ccba6ada008d743f68ea34806276ced
Travis Krebs <tkrebs@ryerson.ca>
2019-02-12 15:01:37 -0500

    Adding airfoil files

---
+++ Runs/Winglet Optimization/vap3_inputmod_wing.m
@@ -0,0 +1,35 @@
+function vap3_inputmod_wing(vap_filename, pan)
+
+str = [];
+fp = fopen(vap_filename);
+while ~feof(fp)
+    str = [str, fgets(fp)];
+end
+fclose(fp);
+
+wing_M = 1;
+str_wing = sprintf(['\n<wing>\n<symmetry>TRUE</symmetry>\n<incidence>0</incidence>\n<trimable>FALSE</trimable>\n<triangular_elements>FALSE</triangular_elements>\n\n<chordwise_elements>', num2str(wing_M), '</chordwise_elements>\n\n<vehicle_x>0</vehicle_x>\n<vehicle_y>0</vehicle_y>\n<vehicle_z>0</vehicle_z>\n']);
+
+str_panel = [];
+for ii = 1:size(pan,2)
+    str_panel = [str_panel sprintf('\n<panel>\n<spanwise_elements>2</spanwise_elements>\n<strip_airfoil>NASA GA(W)-2 Modified</strip_airfoil>\n')];
+
+    for i = 1:size(pan(ii).geom,1)
+       str_panel = [str_panel, sprintf('<section>\n\t<wing_x>%.6f</wing_x>\n\t<wing_y>%.6f</wing_y>\n\t<wing_z>%.6f</wing_z>\n\t<chord>%.6f</chord>\n\t<twist>%.6f</twist>\n\t<camber_airfoil>nan</camber_airfoil>\n</section>\n', pan(ii).geom(i,1), pan(ii).geom(i,2), pan(ii).geom(i,3), pan(ii).geom(i,4), pan(ii).geom(i,5))];      
+    end
+
+    str_panel = [str_panel, sprintf('</panel>\n\n')];
+end
+
+
+
+str_wing = [str_wing, str_panel, sprintf('\n</wing>')];
+
+k = strfind(str, '</vehicle>') - 6;
+str_out = [str(1:k), str_wing, str(k:end)];
+
+fp = fopen(vap_filename, 'w');
+fprintf(fp, str_out);
+fclose(fp);
+
+end
