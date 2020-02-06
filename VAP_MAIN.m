a0f64f927c65581eeed4b31ea7713ab33f56702d
travis-k <tkrebs@ryerson.ca>
2020-02-05 19:02:24 -0500

    Adding split-winglet VAP Test file

--- VAP_MAIN.m
+++ VAP_MAIN.m
@@ -8,14 +8,3 @@ VAP_IN = [];
 VAP_IN.RELAX = 1;
 VAP_IN.valMAXTIME = 40;
 OUTP = fcnVAP_MAIN(filename, VAP_IN);
-
-%%
-% hFig1 = figure(1);
-% clf(1);
-% 
-% plot([OUTP.vecVEHALPHA]',[OUTP.vecPREQ]','--ok')
-% xlabel('Alpha','FontSize',15);
-% ylabel('Preq (W)','FontSize',15);
-% grid minor
-% box on
-% axis tight
