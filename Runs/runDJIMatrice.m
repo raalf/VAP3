516cda4861e16a92be7d8e7dfa112712e4d0a026
Devin <devinbarcelos@gmail.com>
2020-09-21 14:26:48 -0400

    Updating DJI matrice input file with zero lift angle and run file

--- Runs/runDJIMatrice.m
+++ Runs/runDJIMatrice.m
@@ -10,11 +10,17 @@ end
 %% Run fcnVAP over a series of advance ratios and angles
 angles = [-90,-75,-60,-45,-30,-15,0,15,30,45,60,75,90];
 rpm = [2000 3000 4000 5000 6000];
+
 J = 0:0.1:1;
 
 filename = 'inputs/Matrice_210_RTK_Rotor.vap';
-
-OUTP = fcnVAP_MAIN(filename, []);
+CT = nan(length(J),length(angles),length(rpm));
+CP = nan(length(J),length(angles),length(rpm));
+CQ = nan(length(J),length(angles),length(rpm));
+CMx = nan(length(J),length(angles),length(rpm));
+CMy = nan(length(J),length(angles),length(rpm));
+CNx = nan(length(J),length(angles),length(rpm));
+CNy = nan(length(J),length(angles),length(rpm));
 
 parfor i = 1:length(angles)
     AOA_OI = angles(i);
@@ -24,6 +30,7 @@ parfor i = 1:length(angles)
         
         for k = 1:length(rpm)
             VAP_IN = [];
+            VAP_IN.RELAX = false;
             VAP_IN.valMAXTIME = 160;
             VAP_IN.valSTARTFORCES = 140;
             VAP_IN.vecROTORRPM = rpm(k);
@@ -31,7 +38,18 @@ parfor i = 1:length(angles)
             VAP_IN.vecVEHVINF = adv_r*(rpm(k)/60)*0.4572;
             VAP_IN.valDELTIME = 1/((rpm(k)/60)*20); %20 timesteps per rev
             
-            OUTP(i) = fcnVAP_MAIN(filename, VAP_IN);
+            OUTP = fcnVAP_MAIN(filename, VAP_IN);
+            CT(j,i,k) = OUTP.vecCT_AVG;
+            CP(j,i,k) = OUTP.vecCP_AVG;
+            CQ(j,i,k) = OUTP.vecCP_AVG/(2*pi*rpm(k)/60);
+            CMx(j,i,k) = OUTP.vecCMx_AVG;
+            CMy(j,i,k) = OUTP.vecCMy_AVG;
+            CNx(j,i,k) = OUTP.vecCFx_AVG;
+            CNy(j,i,k) = OUTP.vecCFy_AVG;
         end
     end
 end
+
+[Angle, J, RPM] = meshgrid(angles,J,rpm);
+
+save('DJI_Matrice_Data')
